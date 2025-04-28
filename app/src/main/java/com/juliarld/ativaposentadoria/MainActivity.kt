c69e4f7e9a8db12fac4d330faf0e8c0fe1f55d04
J-Ramos104 <juliarld2006@gmail.com>
2025-04-28 14:47:46 -0300

    Alterando o código

--- app/src/main/java/com/juliarld/ativaposentadoria/MainActivity.kt
+++ app/src/main/java/com/juliarld/ativaposentadoria/MainActivity.kt
@@ -29,33 +29,58 @@ class MainActivity : AppCompatActivity() {
         //Associando o adapter ao spineer
         binding.spinner.adapter = adapter
 
+        // Configurando o ícone no TextView do nome
+        binding.textVwNome.setCompoundDrawablesWithIntrinsicBounds(
+            R.drawable.baseline_person_24,0,0,0
+        )
+
         //Configuração ao clicar no botão calcular
         binding.btnCalcular.setOnClickListener {
-            //verificação se o campo está vazio
-            if(binding.editTextText.text.toString().isEmpty()){
-                //Se estiver vazio, é exibido uma mensagem avisando
-                binding.textVwResultado.text = "Idade não informada. Por favor, preencha o campo"
-                //Realiza a interrupção da execução
-                return@setOnClickListener
-            }
+
             //Lendo a idade digitada pelo usuário
-            val idade = binding.editTextText.text.toString().toLong()
+            val idade = binding.editTextText.text.toString().toLongOrNull()
             //Pega o gênero selecionado no Spinner, pelo usuário
-            val sexo = binding.spinner.selectedItem.toString()
+            val sexo = binding.spinner.selectedItem as String
 
-            // Exibindo idade digitada (mensagem rápida)
-            Toast.makeText(this, idade.toString(), Toast.LENGTH_SHORT).show()
+            //Verificando se o campo para preencher a idade está vazia
+            if (idade != null) {
+                var resultado: Long
 
-            //Calculando o tempo para aposentadoria, conforme a idade
-            val result = if (sexo == "Masculino") 65 - idade else 62 - idade
+                // Exibindo idade digitada (mensagem rápida)
+                Toast.makeText(this,"Idade digitada: $idade", Toast.LENGTH_SHORT).show()
 
-            //Exibindo a mensagem do resultado, dependendo do resultado
-            binding.textVwResultado.text = if(result > 0) {
-                "Faltam $result anos para você se aposentar"
-            } else {
-                "Você já tem direito à aposentadoria!"
+                //Verificando o sexo selecionado
+                if(sexo.trim() == "Masculino") {
+                    resultado = 65 - idade
+                } else {
+                    resultado = 62 - idade
+                }
+                //Exibindo a mensagem do resultado, dependendo do resultado
+                if (resultado > 0) {
+                    binding.textVwResultado.text = "Faltam $resultado anos para você se aposentar"
+
+                    //Configurando o icon para que apareça junto com a mensagem
+                    binding.textVwResultado.setCompoundDrawablesWithIntrinsicBounds(
+                        R.drawable.baseline_hourglass_top_24,0,0,0
+                    )
+
+                } else{
+                    binding.textVwResultado.text = "Você já tem direito à aposentadoria"
+
+                    //Configurando o icon para que apareça junto com a mensagem
+                    binding.textVwResultado.setCompoundDrawablesWithIntrinsicBounds(
+                        R.drawable.baseline_stars_24,0,0,0
+                    )
+                }
+            } else{
+                //Mostrando uma mensagem avisando erro caso o campo de digitar a idade esteja vazio
+                binding.textVwResultado.text = "Informe sua idade, por gentileza"
+
+                //Configurando o icon para que apareça junto com a mensagem
+                binding.textVwResultado.setCompoundDrawablesWithIntrinsicBounds(
+                    R.drawable.baseline_info_24,0,0,0
+                )
             }
-
         }
 
     }
